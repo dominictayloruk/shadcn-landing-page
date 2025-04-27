a809820621e6a64505d9104b132f92b441ef1914
Dominic Taylor <dominic@dominictaylor.dev>
2025-04-27 23:35:07 +0100

    Update dark mode styling for navbar border and contact form background

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -49,7 +49,7 @@ export function ContactForm() {
   return (
     <section
       id="contactForm"
-      className="py-16 my-16 sm:my-24 bg-primary bg-[url('data:image/svg+xml,%3Csvg%20xmlns=%22http://www.w3.org/2000/svg%22%20width=%2228%22%20height=%2249%22%20viewBox=%220%200%2028%2049%22%3E%3Cg%20fill-rule=%22evenodd%22%3E%3Cg%20id=%22hexagons%22%20fill=%22%2384cc16%22%20fill-opacity=%220.3%22%20fill-rule=%22nonzero%22%3E%3Cpath%20d=%22M13.99%209.25l13%207.5v15l-13%207.5L1%2031.75v-15l12.99-7.5zM3%2017.9v12.7l10.99%206.34%2011-6.35V17.9l-11-6.34L3%2017.9zM0%2015l12.98-7.5V0h-2v6.35L0%2012.69v2.3zm0%2018.5L12.98%2041v8h-2v-6.85L0%2035.81v-2.3zM15%200v7.5L27.99%2015H28v-2.31h-.01L17%206.35V0h-2zm0%2049v-8l12.99-7.5H28v2.31h-.01L17%2042.15V49h-2z%22/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] bg-repeat bg-fixed"
+      className="py-16 my-16 sm:my-24 bg-primary dark:bg-primary/40 bg-[url('data:image/svg+xml,%3Csvg%20xmlns=%22http://www.w3.org/2000/svg%22%20width=%2228%22%20height=%2249%22%20viewBox=%220%200%2028%2049%22%3E%3Cg%20fill-rule=%22evenodd%22%3E%3Cg%20id=%22hexagons%22%20fill=%22%2384cc16%22%20fill-opacity=%220.3%22%20fill-rule=%22nonzero%22%3E%3Cpath%20d=%22M13.99%209.25l13%207.5v15l-13%207.5L1%2031.75v-15l12.99-7.5zM3%2017.9v12.7l10.99%206.34%2011-6.35V17.9l-11-6.34L3%2017.9zM0%2015l12.98-7.5V0h-2v6.35L0%2012.69v2.3zm0%2018.5L12.98%2041v8h-2v-6.85L0%2035.81v-2.3zM15%200v7.5L27.99%2015H28v-2.31h-.01L17%206.35V0h-2zm0%2049v-8l12.99-7.5H28v2.31h-.01L17%2042.15V49h-2z%22/%3E%3C/g%3E%3C/g%3E%3C/svg%3E')] bg-repeat bg-fixed"
     >
       <div className="flex min-h-[60vh] h-full w-full items-center justify-center px-4">
         <Card className="mx-auto max-w-lg">
